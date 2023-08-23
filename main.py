c3c52bdc6b5c475b71fa9c5c4542bb178f98808f
DamianT7 <Damiant7@live.com>
2023-08-23 17:02:51 +0200

    optimalisations

--- main.py
+++ main.py
@@ -25,12 +25,34 @@ def main():
         service = build("sheets", "v4", credentials=credentials)
         sheets = service.spreadsheets()
 
-        result = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range="Blad1!A1:A6").execute()
+        # Assuming you want to check the first 100 rows (adjust as needed)
+        range_to_check = "Blad1!A2:M101" 
+
+        result = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range=range_to_check).execute()
 
         values = result.get("values", [])
 
-        for row in values:
-            print(values)
+        for i, row in enumerate(values, start=2):
+
+            # Check if A to F filled
+            if len(row) >= 6 and all(row[:6]):
+                print(f"Row {i}: Hotelgegevens ingevuld")
+            else:
+                print(f"Row {i}: Hotelgegevens missing")
+
+            # Check G to K
+            if len(row) >= 11 and any(not cell for cell in row[6:11]): 
+                print(f"Row {i}: VCC missing")
+
+            # Check L
+            if len(row) >= 12 and not row[11]:
+                print(f"Row {i}: VCC not sent to hotel")
+
+            # Check M
+            if len(row) >= 13 and not row[12]:
+                print(f"Row {i}: Old VCC not canceled")
+
+
     except HttpError as error:
         print(error)
 
